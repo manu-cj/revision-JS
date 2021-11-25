f047404a5074c96dc452813ce5083bcf05763e28
manucarenjot <carenjotje@gmail.com>
2021-11-25 11:16:52 +0100

    revision morpion

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,9 @@
 const LEFT_BUTTON = 0;
 const RIGHT_BUTTON = 2;
 
+let joueur1 = true;
+let joueur2 = true;
+
 const cases = document.getElementsByClassName("case");
 
 document.addEventListener('contextmenu', function (event) {
@@ -15,18 +18,22 @@ function test() {
 for (let i = 0; i < cases.length; i++) {
     cases[i].addEventListener('mouseup', function (event) {
         if (event.button === LEFT_BUTTON) {
+            insertText(this, 'X', 'green')
 
 
-
-            insertText(this, 'X', 'green')}
+            }
         else if (event.button === RIGHT_BUTTON)
             insertText(this, 'O', 'red')
+
     });
 }
 
 function insertText(element, lettreJoueur, classCss) {
     element.innerHTML = lettreJoueur;
     element.classList.add(classCss);
+
+
+
 }
 
 
